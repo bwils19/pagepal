822f099a3cd736ac659daa8d1c5bdce7213ebba1
Conor Meade <cmeade6479@gmail.com>
2024-08-01 10:46:16 -0400

    groups stuff, html and js script

--- public/script.js
+++ public/script.js
@@ -96,27 +96,4 @@ document.addEventListener('DOMContentLoaded', () => {
       return false;
     }
   }  
-
-  async function getGroups() {
-    try {
-      const response = await fetch('http://localhost/BookClub/groups.php', {  // THIS NEEDS to be changed from localhost
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      const text = await response.json();
-
-    } catch (error) {
-      console.error('Error getting groups:', error);
-      const data = {
-        "bg_id": 999,
-        "bg_name": "foo group",
-        "bg_current_book_id": 1
-      }
-      return JSON.stringify(data)
-      // return false;
-    }
-  }
 });
